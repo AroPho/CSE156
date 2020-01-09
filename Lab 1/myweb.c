f9a6f8a5ff873a131adaf461f2cda6c8b1fdc84b
Aaron Nguyen <aropho@eduroam-169-233-254-230.ucsc.edu>
2020-01-08 20:31:31 -0800

    Starting of Lab 1

---
+++ Lab 1/myweb.c
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char * argv[]){
+    char * hostname = argv[1];
+    char * port = "80";
+    
+    struct addrinfo hints, *res;
+    int sockfd;
+
+    memset(&hints, 0,sizeof hints);
+    hints.ai_family=AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    getaddrinfo(hostname,port, &hints, &res);
+    sockfd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+    connect(sockfd,res->ai_addr,res->ai_addrlen);
+    char * msg = "Hello";
+    send(sockfd, msg, sizeof(msg), 0)
+    
+
+
+}
