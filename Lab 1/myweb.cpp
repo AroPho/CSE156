3bd455518d9887914ee7c0e5fa192827239c67b9
anguy200 Time Share <aaronn1999@gmail.com>
2020-01-09 14:10:30 -0800

    not sending properly

--- Lab 1/myweb.cpp
+++ Lab 1/myweb.cpp
@@ -1,14 +1,17 @@
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <bits/stdc++.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+using namespace std;
+
 
 int main(int argc, char * argv[]){
     char * hostname = argv[1];
-    char * port = "8080";
+    string port  = "8080";
     
     struct addrinfo hints, *res;
     int sockfd;
@@ -16,13 +19,18 @@ int main(int argc, char * argv[]){
     memset(&hints, 0,sizeof hints);
     hints.ai_family=AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    getaddrinfo(hostname,port, &hints, &res);
+    getaddrinfo(hostname,port.c_str(), &hints, &res);
     sockfd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
     connect(sockfd,res->ai_addr,res->ai_addrlen);
-    char * msg = "Hello";
-    char * get_request = "";
-    send(sockfd, msg, sizeof(msg), 0);
-    
-
-
+    //string  msg = "Hello";
+    int count = 1;
+    int n;
+    char c[1];
+    //char * get_request = "";
+    while((n = read(0, c, 1)) > 0){
+      if(count == 1){
+        send(sockfd, c, 1, 0);
+        count--;
+      }
+    }
 }
