6403cd9bd0f877912f1b9ea95f266241b87601af
Aaron Nguyen <aropho.ucsc.edu>
2020-01-09 15:31:59 -0800

    will this work?

--- Lab 1/myweb.cpp
+++ Lab 1/myweb.cpp
@@ -14,14 +14,18 @@ int main(int argc, char * argv[]){
         warn("Insufficient number of arguements givin");
     }
     char * hostname = argv[1];
-    string port  = "8080";
+    string port  = "80";
     char * path = argv[2];
 
     string str_path(path);
     string file = str_path.substr(str_path.find("/"));
+    if((int first = str_path.find(":") != -1)){
+        int last = str_path.find("/");
+        port = str_path.substr(str_path.find(:) + 1, last - first - 1 );
+    }
 
     string get_request = "GET " + file + " HTTP/1.1\r\nHost: " + hostname + "\r\n\r\n"
-
+    
     struct addrinfo hints, *res;
     int sockfd;
 
@@ -31,15 +35,18 @@ int main(int argc, char * argv[]){
     getaddrinfo(hostname, port.c_str(), &hints, &res);
     sockfd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
     connect(sockfd,res->ai_addr,res->ai_addrlen);
+    send(sockfd, get_request.c_str(), get_request.length(), 0);
     //string  msg = "Hello";
     // int count = 1;
     int numbytes;
     
-    char c[1];
+    char c;
     //char * get_request = "";
-    /*while((numbytes = recv(sockfd, &c, 1, 0)) != 0)*/
-    while(1){
-       read(0, &c, 1);
-       send(sockfd, c, 1, 0);
+    // while(1){
+    //    read(0, &c, 1);
+    //    send(sockfd, c, 1, 0);
+    // }
+    while((numbytes = recv(sockfd, &c, 1, 0)) != 0){
+        printf("%c", c);
     }
 }
