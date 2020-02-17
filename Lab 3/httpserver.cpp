cfb09159c493111d895a8fc1c329caf769188966
Aaron Nguyen <aropho.ucsc.edu>
2020-02-16 17:24:15 -0800

    removed timeout on server

--- Lab 3/httpserver.cpp
+++ Lab 3/httpserver.cpp
@@ -274,8 +274,8 @@ int main(int argc, char * argv[]){
  
     bzero( &servaddr, sizeof(servaddr));
  
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+    servaddr.sin_family = AF_INET; // IPv4 
+    servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(atoi(port));
  
     bind(main_socket, (struct sockaddr *) &servaddr, sizeof(servaddr));
