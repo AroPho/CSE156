c96de9bfbf875414cf79b4542f76b6a35205ecdc
Aaron Nguyen <aropho.ucsc.edu>
2020-02-24 15:36:26 -0800

    Starting lab 4

---
+++ Lab 4/shell.cpp
@@ -0,0 +1,63 @@
+#include <string.h>
+#include <unistd.h> 
+#include <fcntl.h>
+#include <stdio.h> 
+#include <err.h>
+#include <errno.h>
+#include <sys/socket.h> 
+#include <sys/types.h>
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sstream>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/stat.h>
+#include <iostream>
+#include <fstream>
+#include <poll.h>
+#include <signal.h>
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char * argv[]){
+    //Checks for appropriate number of args
+        struct addrinfo hints, *addrs;
+        memset(&hints, 0,sizeof hints);
+        hints.ai_family=AF_UNSPEC;
+        hints.ai_socktype = SOCK_STREAM;
+
+        string hostname = argv[1];
+        string port = argv[2];
+        
+        // cout << port;
+        // printf("%s", hostname.c_str());
+
+        getaddrinfo(hostname.c_str(), port.c_str(), &hints, &addrs);
+        int new_fd = socket(addrs->ai_family, addrs->ai_socktype, addrs->ai_protocol);
+        int numbytes;
+
+        
+        // host_buff = (string*) malloc(sizeof(string)*(num_args*800));
+
+        //cout << 2 << "\n";
+    
+        // exit(1);
+    try{
+        char in_buff[1024];
+        char out_buff[1024];
+        while((numbytes = recv(new_fd, in_buff, 1024, 0)) > 0){
+            cout << "\n" << in_buff;
+            read(0, out_buff, 1024);
+            send(new_fd, out_buff, sizeof(out_buff), 0);
+        }
+
+    }catch(...){
+        warn("Warning internal server error closing connections");
+        string content = "Content-Length: " + to_string(0) + "\r\n\r\n";
+        string header = "HTTP/1.1 500 Created\r\n" + content;
+        send(new_fd, header.c_str(), header.length(), 0);
+    }
+}
