f14cf20ca9d411bcf74b5bef73c177f6506a3b81
Aaron Nguyen <aropho.ucsc.edu>
2020-03-11 12:40:40 -0700

    working on chat mode

--- Lab 5/Chat_Server.cpp
+++ Lab 5/Chat_Server.cpp
@@ -88,8 +88,8 @@ void connect_clients(int sock, string line){
                 break;
             }
         }
-        ip += " " + temp;
-        printf("%s\n", ip.c_str());
+        // ip += " " + temp;
+        // printf("%s\n", ip.c_str());
         send(sock, ip.c_str(), ip.length(), 0);
     }else{
         temp = "Error: " + line.substr(0, line.length() - 4) + " is no longer waiting for a connection or you typed the name wrong\r\n\r\n";
