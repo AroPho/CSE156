7a7d5e0ba6923a12a74f60603042664ca2fb6972
Aaron Nguyen <aropho.ucsc.edu>
2020-03-11 12:11:27 -0700

    working on chat mode

--- Lab 5/Chat_Server.cpp
+++ Lab 5/Chat_Server.cpp
@@ -81,14 +81,16 @@ void connect_clients(int sock, string line){
                 break;
             }
         }
+        string ip = "";
         struct sockaddr_in addr;
         socklen_t addr_size = sizeof(struct sockaddr_in);
         getpeername(other_client, (struct sockaddr *)&addr, &addr_size);
-        string ip = inet_ntoa(addr.sin_addr);
+        ip += inet_ntoa(addr.sin_addr);
 
-        string address = "IP: " + ip + " " + temp;
-        printf("%s\n", address.c_str());
-        send(sock, address.c_str(), address.length(), 0);
+        
+        printf("%s\n", ip.c_str());
+        printf("%s\n", temp.substr(0, temp.length() - 4).c_str());
+        // send(sock, address.c_str(), address.length(), 0);
     }else{
         temp = "Error: " + line.substr(0, line.length() - 4) + " is no longer waiting for a connection or you typed the name wrong\r\n\r\n";
         send(sock, temp.c_str(), temp.length(), 0);
