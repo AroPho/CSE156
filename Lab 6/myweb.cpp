09e5661ee32a8a45cf457319c9ea1b10d4b08ffd
Aaron Nguyen <aropho.ucsc.edu>
2020-03-16 21:45:47 -0700

    ssl_write doesnt work

--- Lab 6/myweb.cpp
+++ Lab 6/myweb.cpp
@@ -110,9 +110,9 @@ void https(int sock, string file, string hostname){
     
     SSL_CTX *ctx = SSL_CTX_new (meth);
     SSL_CTX_set_options(sslctx, SSL_OP_SINGLE_DH_USE);
-    int use_cert = SSL_CTX_use_certificate_file(sslctx, "/serverCertificate.pem" , SSL_FILETYPE_PEM);
+    int use_cert = SSL_CTX_use_certificate_file(ctx, "/serverCertificate.pem" , SSL_FILETYPE_PEM);
 
-    int use_prv = SSL_CTX_use_PrivateKey_file(sslctx, "/serverCertificate.pem", SSL_FILETYPE_PEM);
+    int use_prv = SSL_CTX_use_PrivateKey_file(ctx, "/serverCertificate.pem", SSL_FILETYPE_PEM);
 
 
     SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
