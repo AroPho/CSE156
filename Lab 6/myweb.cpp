5b2221e9904ff8d9602779f082a1c54c619d5269
Aaron Nguyen <aropho.ucsc.edu>
2020-03-16 18:45:14 -0700

    Almost done?

--- Lab 6/myweb.cpp
+++ Lab 6/myweb.cpp
@@ -13,7 +13,47 @@
 #include <sstream>
 #include <iostream>
 #include <fstream>
+
+#include <openssl/applink.c>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <openssl/crypto.h>
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+
 using namespace std;
+
+#define CERT_FILE  HOME "1024ccert.pem"
+#define KEY_FILE  HOME  "1024ckey.pem"
+#define CIPHER_LIST "AES128-SHA"
+#define CA_FILE "/certs/1024ccert.pem"
+#define CA_DIR  NULL
+#define KEY_PASSWD "keypass"
+
+SSL_CTX *sslctx;
+SSL *cSSL;
+
+
+void InitializeSSL()
+{
+    SSL_load_error_strings();
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+}
+
+void DestroySSL()
+{
+    ERR_free_strings();
+    EVP_cleanup();
+}
+
+void ShutdownSSL()
+{
+    SSL_shutdown(cSSL);
+    SSL_free(cSSL);
+}
+
 
 
 int getnthindex(string s, char t, int n)
@@ -49,104 +89,91 @@ int catch_length(string line){
 	return -1;
 }
 
-int main(int argc, char * argv[]){
-    
-    //Checks for appropriate number of args
-    if(argc < 3){
-        printf("ree\n");
+void ssl_communication(int sock, string file, string hostname){
+    sslctx = SSL_CTX_new( TLSv1_2_client_method());
+    SSL_CTX_set_options(sslctx, SSL_OP_SINGLE_DH_USE);
+
+    cSSL = SSL_new(sslctx);
+    SSL_set_fd(cSSL, sock );
+    //Here is the SSL Accept portion.  Now all reads and writes must use SSL
+    ssl_err = SSL_connect(cSSL);
+    if(ssl_err <= 0)
+    {
+        //Error occurred, log and close down ssl
+        ShutdownSSL();
     }
-    bool head_bool = false;
+    try{
 
-    // checks if there is a -h present in args
-    char opt;
-	while((opt = getopt(argc, argv, "h")) != -1){
-		switch(opt){
-			case 'h':
-				head_bool = true;
-				break;
-			case '?':
-				break;
-		}
-	}
-    argc -= optind;
-	argv += optind;
-
-    // Getting port/path/hostname from args
-    char * hostname = argv[0];
-    string port;
-    string path(hostname);
-    int first;
-    int last;
-    string file;
-    if((last = getnthindex(path, '/', 3)) == -1){
-        path += "/";
-    }
-
-    string hostname_str = "";
-    if(path.substr(0,4) == "http"){
-        first = getnthindex(path, ':', 2);
-        if(first != -1){
-            hostname_str = path.substr(0, first); 
-            printf("%d\n", last);
-            path = path.substr(first);
-            last = path.find("/");
-            first = path.find(":");
-            file = path.substr(path.find("/"));
-            port = path.substr(first + 1, last - first - 1);
-        }else{
-            port = "80";
-            first = getnthindex(path, '/', 3);
-            file = path.substr(first);
-            hostname_str = path.substr(0, first);
-        }
-    }else{
-        if((first = path.find(":") ) != -1){
-            hostname_str = path.substr(0, first);
-            last = path.find("/");
-            port = path.substr(path.find(":") + 1, last - first - 1 );
-        }else{
-            hostname_str = path.substr(0, path.find("/"));
-            port = "80";
+        string get_request = "GET " + file + " HTTP/1.1\r\nHost: " + hostname + "\r\n\r\n";
+        string header_send = "HEAD " + file + " HTTP/1.1\r\nHost: " + hostname + "\r\n\r\n";
+        // connect(sockfd,res->ai_addr,res->ai_addrlen);
         
+        // Checks for what type of http request needs to be sent
+        if(head_bool){
+            SSL_write()(cSSL, header_send.c_str(), header_send.length());
         }
-        file = path.substr(path.find("/"));
+        if(!head_bool){
+            SSL_write()(cSSL get_request.c_str(), get_request.length();
+        }
+        // cout << "here";
+        int numbytes;
+        int written = 0;
+        int end_header = 0;
+        int length = -1;
+        string temp;
+        string filename = "output.dat";
+        char c;
+        int fd;
+
+        // If GET request create output.dat file
+        if(!head_bool){
+            remove(filename.c_str());
+            fd = open(filename.c_str(), O_WRONLY | O_CREAT, 0777);
+        }
+        // cout << "here2\n";
+
+        // Starts recieving response from server
+        while((numbytes = SSL_read(cSSL, &c, 1)) != 0){
+            if(end_header != 1){
+              temp += c;
+            }
+
+            //Writes to file if end of header
+            if(end_header == 1){
+                written += write(fd, &c, 1);
+            }
+            if(head_bool){
+                printf("%c", c);
+            }
+            if(written == length){
+                break;
+            }
+
+            // Checks for end of header
+            if(end_header == 0 && temp.length() > 3 && temp.substr(temp.length() - 4) == "\r\n\r\n"){ //Checks for end of header
+                    end_header = 1;
+                    if(head_bool){
+                        break;
+                    }
+                    // cout << "1";
+                    length = catch_length(temp);
+                    // printf("here");
+            }
+        }
+        close(fd);
+        ShutdownSSL()
+        close(sockfd);
+    }catch(...){
+        close(sockfd);
+        warn("Warning internal server error closing connections");
     }
-    printf("%s %s\n", hostname_str.c_str(), port.c_str());
-    exit(0);
+}
 
+void no_https(int sockfd, string file, string hostname){
 
-    
-
-    // Creates appropriate GET and HEAD HTTP Request
     string get_request = "GET " + file + " HTTP/1.1\r\nHost: " + hostname + "\r\n\r\n";
     string header_send = "HEAD " + file + " HTTP/1.1\r\nHost: " + hostname + "\r\n\r\n";
 
-    // struct addrinfo hints, *res;
-    // int sockfd;
-
-    // memset(&hints, 0,sizeof hints);
-    // hints.ai_family=AF_UNSPEC;
-    // hints.ai_socktype = SOCK_STREAM;
-    // getaddrinfo(hostname, port.c_str(), &hints, &res);
-    // sockfd = socket(res->ai_family,res->ai_socktype,res->ai_protocol);
-
-    // Creates and establishes socket connection
-    // struct addrinfo hints, *res;
-    // int sockfd;
-
-    struct addrinfo hints, *addrs;
-	struct sockaddr_storage their_addr;
-	socklen_t addr_size;
-
-    memset(&hints, 0,sizeof hints);
-    hints.ai_family=AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    getaddrinfo(hostname_str.c_str(), port.c_str(), &hints, &addrs);
-    int sockfd = socket(addrs->ai_family,addrs->ai_socktype,addrs->ai_protocol);
-    connect(sockfd,addrs->ai_addr,addrs->ai_addrlen);
-       
-
-    
     try{
         // connect(sockfd,res->ai_addr,res->ai_addrlen);
         
@@ -207,5 +234,104 @@ int main(int argc, char * argv[]){
     }catch(...){
         close(sockfd);
         warn("Warning internal server error closing connections");
+    }
+}
+
+
+
+int main(int argc, char * argv[]){
+    
+    //Checks for appropriate number of args
+    if(argc < 3){
+        printf("ree\n");
+    }
+    bool head_bool = false;
+
+    // checks if there is a -h present in args
+    char opt;
+	while((opt = getopt(argc, argv, "h")) != -1){
+		switch(opt){
+			case 'h':
+				head_bool = true;
+				break;
+			case '?':
+				break;
+		}
+	}
+    argc -= optind;
+	argv += optind;
+
+    // Getting port/path/hostname from args
+    char * hostname = argv[0];
+    string port;
+    string path(hostname);
+    int first;
+    int last;
+    string file;
+    if((last = getnthindex(path, '/', 3)) == -1){
+        path += "/";
+    }
+
+    string hostname_str = "";
+    if(path.substr(0,4) == "http"){
+        first = getnthindex(path, ':', 2);
+        if(first != -1){
+
+            hostname_str = path.substr(0, first); 
+            path = path.substr(first);
+            last = path.find("/");
+            first = path.find(":");
+            file = path.substr(path.find("/"));
+            port = path.substr(first + 1, last - first - 1);
+
+        }else{
+
+            port = "80";
+            first = getnthindex(path, '/', 3);
+            file = path.substr(first);
+            hostname_str = path.substr(0, first);
+
+        }
+    }else{
+        if((first = path.find(":") ) != -1){
+
+            hostname_str = path.substr(0, first);
+            last = path.find("/");
+            port = path.substr(path.find(":") + 1, last - first - 1 );
+
+        }else{
+
+            hostname_str = path.substr(0, path.find("/"));
+            port = "80";
+        
+        }
+        file = path.substr(path.find("/"));
+    }
+    printf("%s %s\n", hostname_str.c_str(), port.c_str());
+
+
+    
+
+    // Creates appropriate GET and HEAD HTTP Request
+
+
+
+    InitializeSSL();
+
+    struct addrinfo hints, *addrs;
+	struct sockaddr_storage their_addr;
+	socklen_t addr_size;
+
+    memset(&hints, 0,sizeof hints);
+    hints.ai_family=AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    getaddrinfo(hostname_str.c_str(), port.c_str(), &hints, &addrs);
+    int sockfd = socket(addrs->ai_family,addrs->ai_socktype,addrs->ai_protocol);
+
+    connect(sockfd,addrs->ai_addr,addrs->ai_addrlen);
+    if(hostname_str.substr(0,5) == "https"){
+        ssl_communication(sockfd, file, hostname);
+    }else{
+        no_https(sockfd, file, hostname);
     }
 }
